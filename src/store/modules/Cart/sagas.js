894491abbfb0ede531873c4524da06d9df3f0ffb
Rafael Guerlak <guerlak@hotmail.com>
2020-03-13 17:16:01 -0300

    out of stock alert

--- src/store/modules/Cart/sagas.js
+++ src/store/modules/Cart/sagas.js
@@ -1,6 +1,7 @@
 import api from '../../../services/api';
 import {call, select, put, all, takeLatest} from 'redux-saga/effects';
 import {addToCartSuccess, updateAmountSuccess} from './actions';
+import {Alert} from 'react-native';
 
 function* addToCart({id}) {
     const prodExists = yield select(state => state.cart.find(p => p.id === id));
@@ -20,6 +21,18 @@ function* updateAmount({id, amount}) {
     if (amount <= 0) {
         return;
     }
+
+    const response = yield call(api.get, '5e67121dff85de370f672ccd');
+    const stock = response.data.stock;
+    const index = stock.findIndex(p => p.id === id);
+    if (stock[index].amount < amount) {
+        Alert.alert(
+            'Sem estoque',
+            'Desculpe, não temos estoque suficientes para quantidade selecionada.',
+        );
+        return;
+    }
+
     yield put(updateAmountSuccess(id, amount));
 }
 
